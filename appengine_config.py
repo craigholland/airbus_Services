0ad593b8bc86e9f2f5a140221d7878789ad8cf07
craigholland <craig_holland@hotmail.com>
2018-07-24 01:46:08 -0700

    Clean Slate

--- appengine_config.py
+++ appengine_config.py
@@ -1,10 +1,6 @@
 """`appengine_config` gets loaded when starting a new application instance."""
-import sys
-import os.path
-
 from google.appengine.ext import vendor
-# add `lib` subdirectory to `sys.path`, so our `main` module can load
-# third-party libraries.
-#sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
-
+# insert `lib` as a site directory so our `main` module can load
+# third-party libraries, and override built-ins with newer
+# versions.
 vendor.add('lib')
--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from flask import Flask
-app = Flask(__name__)
+"""`main` is the top level module  of airbus Services."""
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# Import the Pyramid Framework
+from pyramid.config import Configurator
+from pyramid.response import Response
 
-if __name__ == '__main__':
-    app.run()
+
+config = Configurator()
+
+
+def root_page(request):
+    """Main Page."""
+
+    return Response('Fuck Off World ')
+
+
+_routes = [
+    ('root', '/', root_page)
+]
+
+routes =  _routes
+for route in routes:
+    name, uri, handler = route
+    config.add_route(name, uri)
+    config.add_view(handler, route_name=name)
+
+app = config.make_wsgi_app()
